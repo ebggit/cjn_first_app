ee688c71cdfb77fd04ac4f72fcae34a8e2ebf438
ebggit <git@hidden>
2012-08-06 13:51:41 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '60e0a6b1a62c1e2b7b02e4795f3b0fd3b05bdad2a61254b4fde8460664d175de68b01287b43d58437a65f4a9e3402e8e5e42ad40f91e9d765fe8741f74563c10'
